9db89bdbae1d4e6ff60d911257a2b37257cd4ac1
Coco <coulet.corentin@gmail.com>
2024-01-15 10:31:51 +0100

    ajout d'un onglet contact complet non fonctionnel

--- front/src/pages/contact.jsx
+++ front/src/pages/contact.jsx
@@ -1,6 +1,48 @@
+import '../styles/contact.scss';
+
 const Contact = () => {
     return (
-        <div> Bienvenue sur Contact </div>
+        <form className="contact">
+            <label>
+                Your email*
+                <input
+                type="email"
+                name="email"
+                required
+            />
+            </label>
+            <br />
+            <label>
+                Nom*
+                <input
+                type="text"
+                name="name"
+                required
+                />
+            </label>
+            <br />
+            <label>
+                Prenom*
+                <input
+                type="text"
+                name="surname"
+                required
+                />
+            </label>
+            <label>
+                Sujet
+                <input
+                type="text"
+                name="message"
+                required
+                />
+            </label>
+            <label>
+                Votre message
+            </label>
+            <textarea />
+            <button type="submit">Login</button>
+        </form>  
     )
 }
 
